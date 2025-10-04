b662d0fb7f57442dc6552c515f44f9fc03269ae1
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-04 16:31:38 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+# backend.tf (real-time companies style)
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-backend-all-envs"
+    key            = "jenkins-sonarqube-values/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks-jenkins-sonarqube-values"
+  }
+}
