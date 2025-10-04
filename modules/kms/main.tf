e2047eea1169e816b73ebdb5655a003d44577404
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-04 16:28:14 +0530

    Create main.tf

---
+++ modules/kms/main.tf
@@ -0,0 +1,38 @@
+resource "aws_kms_key" "this" {
+  description             = var.kms_key_name
+  deletion_window_in_days = 10
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowRootAccess"
+        Effect    = "Allow"
+        Principal = { AWS = "arn:aws:iam::584868042761:root" }
+        Action    = "kms:*"
+        Resource  = "*"
+      },
+      {
+        Sid       = "AllowEC2UseOfKey"
+        Effect    = "Allow"
+        Principal = { Service = "ec2.amazonaws.com" }
+        Action    = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:DescribeKey",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = var.kms_key_name
+  }
+}
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/${var.kms_key_name}"
+  target_key_id = aws_kms_key.this.id
+}
