2e8e372c3c64e5687a8a34f4934388e6b694ad66
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-13 02:34:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-backend-all-envs"
     key            = "jenkins-sonarqube-values/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-southeast-2"
     encrypt        = true
     dynamodb_table = "terraform-locks-jenkins-sonarqube-values"
   }
