0fac1f5325be5f7d0d08e9b97d18812e07b00e44
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-25 22:24:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-all-envs"
+    bucket         = "terraform-backend-all-environments"
     key            = "jenkins-sonarqube-values/terraform.tfstate"
     region         = "ap-southeast-2"
     encrypt        = true
